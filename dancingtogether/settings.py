87da0e68c09738892dcc55883aca2e17c0741042
Robert Gardner <bob.hn.gardner@gmail.com>
2018-03-18 22:27:54 -0700

    Add level, time, module to logging

--- dancingtogether/settings.py
+++ dancingtogether/settings.py
@@ -141,9 +141,15 @@ STATICFILES_DIRS = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s: %(message)s',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
         },
     },
     'loggers': {
