42186b409225ff1ed59020ec1f0d0dd9780b3044
Bob Gardner <bob.hn.gardner@gmail.com>
2018-09-09 10:19:35 -0700

    Use HSTS in production

--- dancingtogether/settings/production.py
+++ dancingtogether/settings/production.py
@@ -2,6 +2,11 @@ from .base import *
 
 import os
 
+# https://docs.djangoproject.com/en/2.1/ref/middleware/#http-strict-transport-security
+
+SECURE_HSTS_SECONDS = os.environ['DT_SECURE_HSTS_SECONDS']
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
 # Sentry Error Reporting
 
 RAVEN_CONFIG = {
