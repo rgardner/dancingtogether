94e55eb11a2f4227271f3ef6a9af53e31c5d7f65
Bob Gardner <bob.hn.gardner@gmail.com>
2019-12-27 13:24:07 -0800

    Upgrade backend formatting

--- radio/exceptions.py
+++ radio/exceptions.py
@@ -4,7 +4,6 @@ class Error(Exception):
 
 class ClientError(Error):
     """Caught by WebSocket receive() handler and returned to client."""
-
     def __init__(self, code, message):
         self.code = code
         self.message = message
